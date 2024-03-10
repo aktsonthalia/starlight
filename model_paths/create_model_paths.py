2ae85205320f48c2ca9c81460ee355e82106cfc3
Ankit Sonthalia <ankitsonthalia24@gmail.com>
2024-03-10 21:09:59 +0100

    create model paths

---
+++ model_paths/create_model_paths.py
@@ -0,0 +1,29 @@
+# takes the name of the folder and creates lists of paths
+
+import os
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--folder', '-f', type=str)
+args = parser.parse_args()
+folder = args.folder
+
+def create_model_paths(folder, output_file):
+    # Get the list of files in the folder
+    files = os.listdir(folder)
+    # Create a list of paths
+    paths = [os.path.join(folder, file) for file in files]
+
+    # Write the list of paths to a file
+    with open(output_file, 'w') as file:
+        for path in paths:
+            file.write(path + '\n')
+
+anchors_folder = os.path.join(folder, "anchors")
+output_file = folder.split('/')[-1] + "_anchors.txt"
+create_model_paths(anchors_folder, output_file)
+
+held_out_folder = os.path.join(folder, "held_out")
+output_file = folder.split('/')[-1] + "_held_out.txt"
+create_model_paths(held_out_folder, output_file)
