da5e10c2d9041278ba1b3bdf2d373364e962bd39
Ankit Sonthalia <53793862+aktsonthalia@users.noreply.github.com>
2024-05-16 12:08:12 +0200

    Update __init__.py

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,11 +1,11 @@
-from .ag_news import load_ag_news
+# from .ag_news import load_ag_news
 from .cifar10 import load_cifar10
 from .cifar100 import load_cifar100
 from .imagenet import load_imagenet1k
 from .mnist import load_mnist
 
 datasets_dict = {
-    "ag_news": load_ag_news,
+    # "ag_news": load_ag_news,
     "cifar10": load_cifar10,
     "cifar100": load_cifar100,
     "imagenet1k": load_imagenet1k,
